1886c7dc6ed1063c99b611dfd4ee13c60ae2bc8c
Sean <sean@vestaridefair.com>
2021-04-01 12:09:53 -0700

    first

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main className="main">
+        <h1 className="title">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
